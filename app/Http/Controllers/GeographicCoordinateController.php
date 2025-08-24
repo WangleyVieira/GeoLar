2fd7d206e70cd533eb5ca454438d0df8022cd971
WangleyVieira <wangleymsv10@hotmail.com>
2025-08-23 20:40:27 -0400

    feat(core): add Models, Migrations and Controller for Owner, Property, PropertyOwner and GeographicCoordinate

---
+++ app/Http/Controllers/GeographicCoordinateController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GeographicCoordinate;
+use Illuminate\Http\Request;
+
+class GeographicCoordinateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GeographicCoordinate  $geographicCoordinate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GeographicCoordinate $geographicCoordinate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GeographicCoordinate  $geographicCoordinate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GeographicCoordinate $geographicCoordinate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GeographicCoordinate  $geographicCoordinate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GeographicCoordinate $geographicCoordinate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GeographicCoordinate  $geographicCoordinate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GeographicCoordinate $geographicCoordinate)
+    {
+        //
+    }
+}
