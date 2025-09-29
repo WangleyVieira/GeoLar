bda16cccc17ff5fe58aa2102ba59dd7e9e4dfb4e
WangleyVieira <wangleymsv10@hotmail.com>
2025-09-29 08:51:28 -0400

    feat(audit): implementation of audit in Models

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Attachment extends Model
+class Attachment extends Model implements Auditable
 {
     use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
     protected $fillable = ['file_name', 'description', 'owner_id', 'registered_by'];
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
--- app/Models/ProfilePhoto.php
+++ app/Models/ProfilePhoto.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class ProfilePhoto extends Model
+class ProfilePhoto extends Model implements Auditable
 {
     use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
     protected $fillable = ['name_original','name_hash', 'user_id', 'registeredByUser', 'inactivatedByUser'];
 
     protected $guarded = ['id', 'created_at', 'update_at'];
